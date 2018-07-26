e50cfce24a314992efb3e46077bb2f7c9089ce6d
Preetam Joshi <pritam.joshi15@gmail.com>
2018-07-26 15:09:38 +0530

    calling static displays method

--- HelloGit.java
+++ HelloGit.java
@@ -1,11 +1,12 @@
 class Hello
 {
-    void myMethod()
+    static void displays()
    {
   sysout("display time");
    }
 	public static void main(String arg[])
 	{
-		System.out.println("Hello world..Welcome to Git ");
+	System.out.println("Hello world..Welcome to Git ");
+	displays();
 	}
 }
